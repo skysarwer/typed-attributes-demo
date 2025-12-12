2c0ca2c4e457c48b86b64d310ffad07c562ca158
evan_works <contact@evan.works>
2025-12-11 23:10:34 -0500

    first commit

---
+++ typed-attributes-demo.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name:       Typed Attributes Demo
+ * Description:       A simple block demonstrating dynamically typed block attributes
+ * Version:           0.1.0
+ * Requires at least: 6.7
+ * Requires PHP:      7.4
+ * Author:            Evan Buckiewicz
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       typed-attributes-demo
+ * Domain Path:       gtsu
+ *
+ * @package           gutenberg-ts-utils
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function gutenberg_ts_utils_typed_attributes_demo_block_init() {
+	register_block_type_from_metadata( __DIR__ . '/build' );
+}
+add_action( 'init', 'gutenberg_ts_utils_typed_attributes_demo_block_init' );
